97cc4a7c61fce27833c357bddb34d53bc7afd32d
yujung00 <ung5468@naver.com>
2022-06-29 18:24:12 +0900

    # 컴포넌트 최적화 -> should component update 추가

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,14 @@ import React, {Component} from "react";
 import './TodoItem.css';
 
 class TodoItem extends Component{
+
+    // 컴포넌트가 리렌더링을 할지 말지 정해줌!
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+
+        // 하단에 조건을 정해서 Return 해주면됨! default 값은 true!
+        return this.props.checked !== nextProps.checked;
+    }
+
     render() {
 
         const {text, checked, id, onToggle, onRemove} = this.props;
--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -2,6 +2,14 @@ import React, {Component} from "react";
 import TodoItem from "./TodoItem";
 
 class TodoItemList extends Component{
+
+    // 컴포넌트가 리렌더링을 할지 말지 정해줌!
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+
+        // 하단에 조건을 정해서 Return 해주면됨! default 값은 true!
+        return this.props.todos !== nextProps.todos;
+    }
+
     render() {
         const {todos, onToggle, onRemove} = this.props;
 
